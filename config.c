371799ea84389d157b0ed63d3e2642d445e5f586
yf <--->
2023-07-27 22:29:25 +0800

    修改音量脚本每5秒刷新一次

--- config.c
+++ config.c
@@ -7,7 +7,7 @@
 Block blocks[] = {
 	{PATH("icon.sh"),              0,              1},
 	{PATH("netspeed.sh"),          1,              8},
-	{PATH("volume.sh"),            0,              2},
+	{PATH("volume.sh"),            5,              2},
 	{PATH("bluetooth.sh"),         15,             3},
 	{PATH("battery.sh"),           60,             4},
 	{PATH("time.sh"),              1,              5},
